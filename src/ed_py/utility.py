ad79cb5d269b0f5d75832501c1be7a3520e64712
Loy van Beek <loy.vanbeek@gmail.com>
2021-08-17 20:58:17 +0200

    Use right timestamps and frame_ids

--- src/ed_py/utility.py
+++ src/ed_py/utility.py
@@ -12,7 +12,8 @@ def rooms_of_entity(wm: WM, entity: Entity) -> List[Entity]:
     """
     Determine all rooms that the entity is in
     """
-    all_rooms = wm.get_entities("room")
+    all_rooms = wm.get_entities(VectorStamped.from_xyz(0, 0, 0, rospy.Time(0), "map"),
+                                etype="room")
     rooms = []
     entity_point = VectorStamped.from_framestamped(entity.pose)
     for room in all_rooms:
@@ -26,18 +27,19 @@ def rooms_of_volume(wm: WM, entity: Entity, volume_id: str) -> List[Entity]:
     """
     Determine all the rooms a volume of an entity is in
     """
-    all_rooms = wm.get_entities(VectorStamped.from_xyz(0, 0, 0, rospy.Time.now(), "map"),
+    all_rooms = wm.get_entities(VectorStamped.from_xyz(0, 0, 0, rospy.Time(0), "map"),
                                 etype="room")
     rooms = []
     try:
         volume = entity.volumes[volume_id]
-        volume_point = VectorStamped(volume.center_point, rospy.Time.now(), entity.uuid)
+        volume_point = VectorStamped(volume.center_point, rospy.Time(0), entity.uuid)
+        volume_point_in_map = wm.tf_buffer.transform(volume_point, "map")
     except KeyError as e:
         rospy.logerr(f"Entity({entity.uuid}) has not a volume with the name: {volume_id}\n{e}")
         return []
 
     for room in all_rooms:
-        if room.in_volume(volume_point, "in"):
+        if room.in_volume(volume_point_in_map, "in"):
             rooms.append(room)
 
     return rooms
