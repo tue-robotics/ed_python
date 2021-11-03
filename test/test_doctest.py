40cb9cc83491caa7697aecd79f487eb128229177
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-11-03 17:53:37 +0100

    Run doctests

---
+++ test/test_doctest.py
@@ -0,0 +1,15 @@
+from test_tools.doctest_unittest import _TestDocTests
+import unittest
+
+
+class DocTestsRobotSmachStates(_TestDocTests):
+    def __init__(self, method_name="test_doctests"):
+        super(DocTestsRobotSmachStates, self).__init__(
+            pkg_name="ed_python", module_name="ed_py", method_name=method_name
+        )
+
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(DocTestsRobotSmachStates())
+    unittest.TextTestRunner(verbosity=2).run(suite)
