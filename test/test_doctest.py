3d2ef624b43eda6fe453fcca154643a40625b894
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-04-17 12:38:55 +0200

    Fix name of doctest class

--- test/test_doctest.py
+++ test/test_doctest.py
@@ -2,12 +2,12 @@ from test_tools.doctest_unittest import _TestDocTests
 import unittest
 
 
-class DocTestsRobotSmachStates(_TestDocTests):
+class DocTestsED(_TestDocTests):
     def __init__(self, method_name="test_doctests"):
-        super(DocTestsRobotSmachStates, self).__init__(pkg_name="ed_python", module_name="ed", method_name=method_name)
+        super(DocTestsED, self).__init__(pkg_name="ed_python", module_name="ed", method_name=method_name)
 
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    suite.addTest(DocTestsRobotSmachStates())
+    suite.addTest(DocTestsED())
     unittest.TextTestRunner(verbosity=2).run(suite)
